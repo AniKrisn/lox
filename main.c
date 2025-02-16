6b3b8b11a9303506314ff71c1ffea4ce93defb26
AniKrisn <anirudh.krishnan@hotmail.com>
2025-02-16 14:45:13 +0530

    init repl

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "common.h"
@@ -8,10 +10,45 @@
 #include "value.h"
 #include "vm.h"
 
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char* argv[]) {
     clock_t start = clock();
 
     initVM();
+
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox[path]\n");
+        exit(64);
+    }
+
+
+
 
     Chunk chunk;
     initChunk(&chunk);
